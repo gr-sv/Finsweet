968a855eb7d1658c7622dc529d6faba2dfb315a1
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-01-21 16:29:50 +0300

    Add logo, menu and copyright into footer

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,26 @@
+import Logo from "./logo";
+import Menu from "./menu";
+
 function Footer () {
 	return (
-		<div>Footer</div>
+		<div className="footer">
+			<div className="wrapper footer-wrapper">
+				<section className="footer-logo">
+					<h2 className="visually-hidden">Logo</h2>
+					<Logo className="footer-logo" />
+				</section>
+
+				<section className="footer-navigation">
+					<h2 className="visually-hidden">Navigation</h2>
+					<Menu />
+				</section>
+
+				<section className="copygight">
+					<h2 className="visually-hidden">Copyright</h2>
+					<p className="copyrigth__tex">&copy; 2021 Finsweet | All rights reserved.</p>
+				</section>
+			</div>			
+		</div>
 	)
 }
 
