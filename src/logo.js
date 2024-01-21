ba87b4b2959312492dd8dfafb3542b3923ad4ae4
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-01-21 16:08:30 +0300

    Delete old logo, update package-lock.json and package.json

--- src/logo.js
+++ src/logo.js
@@ -1,7 +1,6 @@
 function Logo () {
 	return (
 		<a className="logo logo__link navigation__link">
-			{/* как правильно ставить изображение в react? */}
 			<img className="logo__image" alt="Logo of cafe Finsweet" />
 		</a>
 	)
