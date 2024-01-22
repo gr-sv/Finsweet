dbd9e87d757d11a6b58d373234385c9a1337a889
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-01-22 23:56:16 +0300

    Add social and contact-us sections into footer

--- src/footer.js
+++ src/footer.js
@@ -5,19 +5,38 @@ function Footer () {
 	return (
 		<div className="footer">
 			<div className="wrapper footer-wrapper">
-				<section className="footer-logo">
-					<h2 className="visually-hidden">Logo</h2>
-					<Logo className="footer-logo" />
-				</section>
-
 				<section className="footer-navigation">
 					<h2 className="visually-hidden">Navigation</h2>
+					<Logo className="footer-logo" />
 					<Menu className="footer-menu" />
 				</section>
 
 				<section className="copygight">
 					<h2 className="visually-hidden">Copyright</h2>
-					<p className="copyrigth__tex">&copy; 2021 Finsweet | All rights reserved.</p>
+					<p className="copyrigth__text">&copy; 2021 Finsweet | All rights reserved.</p>
+				</section>
+
+				<section className="social">
+				<h2 className="visually-hidden">Social networks</h2>
+				<div className="social__list">
+					<a className="social__link" href="#">
+						<span className="visually-hidden">Facebook</span>
+					</a>
+					<a className="social__link" href="#">
+						<span className="visually-hidden">Twitter</span>
+					</a>
+					<a className="social__link" href="#">
+						<span className="visually-hidden">Instagram</span>
+					</a>
+					<a className="social__link" href="#">
+						<span className="visually-hidden">LinkedIn</span>
+					</a>
+				</div>
+				</section>
+
+				<section className="contact-us">
+					<h2 className="visually-hidden">Contact us</h2>
+					<p className="contact-us__text">Contact us</p>
 				</section>
 			</div>			
 		</div>
