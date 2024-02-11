e85e1333549a78ca9081b49a74bfc822399fe0c7
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-02-11 18:45:43 +0300

    Rename classNames in Header.jsx

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,16 +3,16 @@ import Wrapper from "../wrapper/wrapper.js";
 import Logo from '../../logo.js'
 import Menu from '../../menu.js'
 import Button from "../shared/button/button.js";
-import './header.module.scss'
+import s from './header.module.scss'
 
 function Header () {
 	return (
-		<div className="header">
-			<Wrapper className="header-wrapper">
-				<nav className="navigation">
-					<Logo className="navigation__logo" />
-					<Menu className="navigation__menu" />
-					<Button button__clone>Clone Project</Button>
+		<div className={s.header}>
+			<Wrapper className={s.wrapper}>
+				<nav className={s.navigation}>
+					<Logo className={s.navigation__logo} />
+					<Menu className={s.navigation__menu} />
+					<Button button_clone>Clone Project</Button>
 				</nav>
 			</Wrapper>
 		</div>
