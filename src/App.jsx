8319ea67a6f5e87375d08c93d3bd2c4657f19fec
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-02-21 17:03:00 +0300

    Add folder pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/widgets/header/header.jsx';
 import Footer from './components/widgets/footer/footer.jsx';
-import Home from './components/home/home.jsx';
+import Home from './Pages/home/home.jsx';
 
 function App() {
   return (
