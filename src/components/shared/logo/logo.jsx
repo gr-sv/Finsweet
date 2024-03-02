8c53aa679f27b1f39c0d9c01fcefbdabddf585ee
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-03-02 18:53:23 +0300

    Add corrections in Logo

--- src/components/shared/logo/logo.jsx
+++ src/components/shared/logo/logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classNames from 'classnames';
-import s from './logo.module.scss'
+import s from './logo.module.scss';
 import LogoHeader from '../../../assests/icons/logo-header.svg'
 
 function Logo ({ className }) {
+
 	return (
-		<a className={classNames(s.logo__link, className)}>
+		<a className={classNames(s.link, className)}>
 			<img className={s.logo__image} src={LogoHeader} alt="Logo of cafe Finsweet" />
 		</a>
 	)
