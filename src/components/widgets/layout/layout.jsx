49dc078cc3fe2725de76569e542662445577dcaa
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-04-25 22:34:36 +0300

    Import createBrouserRouter and RouterProvider, delete tag main from Layote

--- src/components/widgets/layout/layout.jsx
+++ src/components/widgets/layout/layout.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import s from './layout.module.scss'
+
+// const router = createBrowserRouter([]);
 
 function Layout ({ children }) {
 	return (
 		<div className={s.layout}>
 			<Header />
-			<main>
-				{children}	
-			</main>						
+			{children}				
 			<Footer />
 		</div>
 	);
