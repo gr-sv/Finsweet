8b404ac880310feffe38c422d7fe217770ee69d0
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-04-25 22:48:10 +0300

    Add page MenuPgae and its styles

---
+++ src/Pages/menuPage/menuPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from './homePage.module.scss'
+
+function MenuPage () {
+	return (
+		<div className={s.menuPage}>
+			<main>
+				<h1>Menu Page</h1>
+			</main>		
+		</div>
+	);
+}
+
+export default MenuPage;
