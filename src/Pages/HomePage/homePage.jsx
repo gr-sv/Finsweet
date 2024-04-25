10e912019d76c5376681f7cdd21367c92290c5a7
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-04-25 22:42:36 +0300

    Add new file homePage and its styles

---
+++ src/Pages/HomePage/homePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from './homePage.module.scss'
+
+function HomePage () {
+	return (
+		<div className={s.homePage}>
+			<main>
+				<h1>Home Page</h1>
+			</main>		
+		</div>
+	);
+}
+
+export default HomePage;
