a6cabb6617d2384f3356e71793c82c68a1081090
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-05-01 23:46:40 +0300

    Add corrections in Button

--- src/components/shared/button/button.jsx
+++ src/components/shared/button/button.jsx
@@ -9,7 +9,7 @@ const Button = ({
 	button_menu,
 	button_form,
 	button_contact,
-	// button_burger,
+	button_burger,
 	disabled,
 	...otherProps
 }) => {
@@ -22,7 +22,7 @@ const Button = ({
 				[s.button_form]: button_form,
 				[s.button_contact]: button_contact,
 				[s.disabled]: disabled,
-				// [s.button_burger]: button_burger,
+				[s.button_burger]: button_burger,
 		})}>
 			{ children }
 		</button>
