7114cc041fcd708894e1beb2c54c23580d3a3504
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-05-04 21:54:43 +0300

    Rename folder pages

--- src/Pages/menuPage/menuPage.jsx
+++ src/Pages/menuPage/menuPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from './homePage.module.scss'
+import s from './menuPage.module.scss'
 
 function MenuPage () {
 	return (
--- src/components/widgets/layout/layout.jsx
+++ src/components/widgets/layout/layout.jsx
@@ -4,13 +4,19 @@ import Header from '../header/header';
 import Footer from '../footer/footer';
 import s from './layout.module.scss';
 
-import HomePage from '../../../Pages/homePage/homePage';
+import HomePage from '../../../pages/homePage/homePage';
+import MenuPage from '../../../pages/menuPage/menuPage';
 
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <HomePage />,
+		errorElement: <div>404 Not Found</div>
+	},
+	{
+		path: '/menu',
+		element: <MenuPage />
 	}
 ]);
 
