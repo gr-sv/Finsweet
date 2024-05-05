4d4b71c5c115453746ddfb5fb088e085cc33c732
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-05-05 23:47:47 +0300

    Add main styles into index.scss

--- src/Pages/HomePage/homePage.jsx
+++ src/Pages/HomePage/homePage.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import s from './homePage.module.scss';
 
 function HomePage () {
 	return (
-		<main className={s.homePage}>
+		<main>
 			<h1>Home Page</h1>
 		</main>
 	);
--- src/Pages/menuPage/menuPage.jsx
+++ src/Pages/menuPage/menuPage.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import s from './menuPage.module.scss'
 
 function MenuPage () {
 	return (
-		<main className={s.menuPage}>
+		<main>
 			<h1>Menu Page</h1>
 		</main>
 	);
