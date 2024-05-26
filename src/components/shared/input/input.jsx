58b5ef4ccaae0733a2e1a11f922771e8a3150f88
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-05-26 03:00:45 +0300

    Add function Input, style file for Input

--- src/components/shared/input/input.jsx
+++ src/components/shared/input/input.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import classNames from 'classnames';
+import s from './input.module.scss';
 
-function Input () {
-
+function Input ({
+	id,
+	className,
+	...otherProps
+}) {
+	return (
+		<input
+			{...otherProps}
+			id={id}
+			name={id}
+			className={classNames(
+				s.input,
+				className,
+		)}/>
+	)
 }
 
 export default Input;
