2cc59977214ff8040b556d87a5e1504ff5fc7b99
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-07-14 22:32:52 +0300

    Add classnames and grids

--- src/components/widgets/knowMore/knowMore.jsx
+++ src/components/widgets/knowMore/knowMore.jsx
@@ -7,38 +7,41 @@ import s from './knowMore.module.scss'
 
 function KnowMore () {
 	return (
-		<section>
-			<Wrapper>
-				<div className="text-wrapper">
-					<h2>Know more about us</h2>
-					<p>We source sustainable & line caught Foods</p>
-					<p>Edit this text to make it your own. To edit, simply 
+		<section className={s.knowMore}>
+			<Wrapper className={s.wrapper}>
+				<div className={s.textWrapper}>
+					<h2 className={`title ${s.knowMore__title}`}>Know more about us</h2>
+					<p className={`intro ${s.knowMore__intro}`}>We source sustainable & line caught Foods</p>
+					<p className={s.knowMore__text}>Edit this text to make it your own. To edit, simply 
 						click directly on the text to start adding your own words. 
 						You can move the text by dragging and dropping the text anywhere 
 						on the page.</p>
 
-					<div className="feature">
-						<h3>10+ People</h3>
-						<p>We are Small Team</p>
-						<p>Through True Rich Attended does no end it his mother since favourable.</p>
+					<div className={s.features}>
+						<div className={`${s.feature} ${s.feature_first}`}>
+							<h3 className={s.feature__title}>10+ People</h3>
+							<p className={s.feature__text}>We are Small Team</p>
+							<p className={s.feature__text}>Through True Rich Attended does no end it his mother since favourable.</p>
+						</div>
+						
+						<div className={`${s.feature} ${s.feature_second}`}>
+							<h3 className={s.feature__title}>2014</h3>
+							<p className={s.feature__text}>We are From</p>
+							<p className={s.feature__text}>Through True Rich Attended does no end it his mother since favourable.</p>
+						</div>
+						
+						<div className={`${s.feature} ${s.feature_third}`}>
+							<h3 className={s.feature__title}>200k</h3>
+							<p className={s.feature__text}>Served</p>
+							<p className={s.feature__text}>Through True Rich Attended does no end it his mother since favourable.</p>
+						</div>
 					</div>
-					
-					<div className="feature">
-						<h3>2014</h3>
-						<p>We are From</p>
-						<p>Through True Rich Attended does no end it his mother since favourable.</p>
-					</div>
-					
-					<div className="feature">
-						<h3>200k</h3>
-						<p>Served</p>
-						<p>Through True Rich Attended does no end it his mother since favourable.</p>
-					</div>					
+										
 					
 					<Link className={`orangeBtnLink ${s.knowMore__link}`} to="/menu">View Menu</Link>
 				</div>
 				
-				<div className="image-wrapper">
+				<div className={s.imageWrapper}>
 					<Image
 						className={`${s.knowMore__image} ${s.image_first}`}
 						src='/assests/images/know-more_1.jpeg'
@@ -48,7 +51,7 @@ function KnowMore () {
 					/>
 
 					<Image
-						className={`${s.knowMore__image} ${s.image_first}`}
+						className={`${s.knowMore__image} ${s.image_second}`}
 						src='/assests/images/know-more_2.jpeg'
 						alt='Know more image 2'
 						width={240}
