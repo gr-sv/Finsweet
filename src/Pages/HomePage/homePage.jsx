04aec233d9f07a35324d75240e576c1e2b1f9570
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-07-22 15:54:52 +0300

    Add new section ourMenu

--- src/Pages/HomePage/homePage.jsx
+++ src/Pages/HomePage/homePage.jsx
@@ -3,6 +3,7 @@ import Header from '../../components/widgets/header/header';
 import Footer from '../../components/widgets/footer/footer';
 import Promo from '../../components/widgets/promo/promo';
 import KnowMore from '../../components/widgets/knowMore/knowMore';
+import OurMenu from '../../components/widgets/ourMenu/ourMenu';
 import s from './homePage.module.scss';
 
 
@@ -13,6 +14,7 @@ function HomePage () {
 			<main>
 				<Promo />
 				<KnowMore />
+				<OurMenu />
 			</main>
 			<Footer />
 		</React.Fragment>
---
+++ src/components/widgets/ourMenu/ourMenu.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Wrapper from "../wrapper/wrapper";
+
+function OurMenu () {
+	return (
+		<section>
+			<Wrapper>
+				<h2>Our menu</h2>
+				<p>Discover our menu chart</p>
+				<p>Through True Rich Attended does no end it his mother since favourable 
+					real had half every him case in packages enquire we up ecstatic.. 
+					Through True Rich Attended does no end it his mother</p>
+				<p>Most Popular Picks</p>
+				<div>
+					<div>
+						<h3>Drp Cofee</h3>
+						<p>$08.85</p>
+					</div>
+					<div>
+						<h3>Choco Cup Cake</h3>
+						<p>$4.85</p>
+					</div>
+					<div>
+						<h3>Chicken Burger</h3>
+						<p>$8.85</p>
+					</div>
+					<div>
+						<h3>French Fries</h3>
+						<p>$7.85</p>
+					</div>
+					<div>
+						<h3>Sandwitch</h3>
+						<p>$3.97</p>
+					</div>
+					<div>
+						<h3>Chinese Soup</h3>
+						<p>$8.85</p>
+					</div>
+				</div>
+			</Wrapper>
+			
+		</section>
+	)
+}
+
+export default OurMenu;
