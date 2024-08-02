5df7204f009d282fe871906490133a8385d27e84
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-08-02 03:03:51 +0300

    Add content onto Quote

--- src/components/widgets/quote/quote.jsx
+++ src/components/widgets/quote/quote.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Wrapper from "../wrapper/wrapper";
 
+import s from './quote.module.scss'
+
 function Quote () {
 	return (
-		<section>
-			<Wrapper>
+		<section className={`section ${s.quote}`}>
+			<Wrapper className={s.wrapper}>
 				<h2>Quote</h2>
+				<blockquote>
+					<p>Edit this text to make it your own. To edit, simply 
+						click directly on the text to start adding your own 
+						words. You can move the text by dragging and dropping 
+						the text</p>
+					<p>Joheny Andro</p>
+					<p>Bhubaneswar, Odisha</p>
+				</blockquote>
 			</Wrapper>
 		</section>
 	)
