74fd810038289021b19a504fa62c481ec58471d8
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-09-19 03:31:19 +0300

    Add preventDefault into burger

--- src/components/widgets/header/header.jsx
+++ src/components/widgets/header/header.jsx
@@ -15,8 +15,8 @@ function Header () {
 					<Logo logoHeader className={s.navigation__logo} />
 
 					<div className={`${s.burger} ${isActive ? s.active : ''}`} onClick={() => setIsActive(false)}>
-						<Navigation headerNav className={s.navigation__links} onClick={e => e.stopPropagation()} />
-						<Button button_clone onClick={e => e.stopPropagation()}>Clone Project</Button>
+						<Navigation headerNav className={s.navigation__links} onClick={e => {e.stopPropagation(); e.preventDefault()}} />
+						<Button button_clone onClick={e => {e.stopPropagation(); e.preventDefault()}}>Clone Project</Button>
 					</div>
 
 					<Button 
