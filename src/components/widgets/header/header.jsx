2ff176281bf1d66995ed551c54d5c5bc1d801947
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-09-20 14:19:52 +0300

    Add div for closing burger menu

--- src/components/widgets/header/header.jsx
+++ src/components/widgets/header/header.jsx
@@ -14,9 +14,12 @@ function Header () {
 				<nav className={s.navigation}>
 					<Logo logoHeader className={s.navigation__logo} />
 
+					
 					<div className={`${s.burger} ${isActive ? s.active : ''}`} onClick={() => setIsActive(false)}>
 						<Navigation headerNav className={s.navigation__links} onClick={e => {e.stopPropagation(); e.preventDefault()}} />
 						<Button button_clone onClick={e => {e.stopPropagation(); e.preventDefault()}}>Clone Project</Button>
+
+						<div className={s.closeBurger} />
 					</div>
 
 					<Button 
